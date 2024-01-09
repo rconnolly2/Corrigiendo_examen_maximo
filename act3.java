7f5f21e4e8a304f9ff8bc0cd7766c771a524161a
Robert Connolly <robertoconnolly100@gmail.com>
2024-01-09 12:13:58 +0100

    es par

---
+++ act3.java
@@ -0,0 +1,21 @@
+
+public class act3 {
+
+    public static boolean esParell(int n) {
+        if (n%2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+        
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(esParell(0));
+      
+        System.exit(0); // fin del programa
+    }
+}
